0ab769741ab0aa3e761af3fe88dfcef2d71b5f65
Eleanor112 <108511811+Eleanor112@users.noreply.github.com>
2023-05-04 18:27:05 +0800

    Update helloworld.cpp

--- HelloWorld/helloworld.cpp
+++ HelloWorld/helloworld.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() {
-	cout << "helloworld --- 012" << endl;
-	cout << "helloworld --- 097" << endl;
+	cout << "helloworld --- 012 ------" << endl;
+	cout << "helloworld --- 097 -------- " << endl;
 	return 0;
 }
